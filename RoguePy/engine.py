179b5b287b7756b2810e820beaecb8e3b9523c80
Eric D. Bailey <eric.bailey@outlook.com>
2020-08-29 19:34:14 -0700

    Updated project to match the 2020 version of the tutorial.  This is updates for Parts 1-3

--- RoguePy/engine.py
+++ RoguePy/engine.py
@@ -1,62 +1,35 @@
-﻿import tcod as libtcod
+﻿from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
 
 from entity import Entity
-from input_handlers import handle_keys
-from map_objects.game_map import GameMap
-from render_functions import clear_all, render_all
+from game_map import GameMap
+from input_handlers import EventHandler
 
-def main():
-    screen_width = 80
-    screen_height = 50
-    map_width = 80
-    map_height = 45
 
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
+class Engine:
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
+        self.entities = entities
+        self.event_handler = event_handler
+        self.game_map = game_map
+        self.player = player
 
-    colors = {
-        'dark_wall' : libtcod.Color(0, 0, 100),
-        'dark_ground' : libtcod.Color(50, 50, 150)
-    }
+    def handle_events(self, events: Iterable[Any]) -> None:
+        for event in events:
+            action = self.event_handler.dispatch(event)
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
-    entities = [npc, player]
+            if action is None:
+                continue
 
-    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+            action.perform(self, self.player)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
-    con = libtcod.console_new(screen_width, screen_height)
+    def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
 
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+        for entity in self.entities:
+            console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
-    key = libtcod.Key()
-    mouse = libtcod.Mouse()
+        context.present(console)
 
-    while not libtcod.console_is_window_closed():
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
-        libtcod.console_flush()
-        clear_all(con, entities)
-
-        action = handle_keys(key)
-
-        move = action.get('move')
-        exit = action.get('exit')
-        fullscreen = action.get('fullscreen')
-
-        if move:
-            dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx, dy)
-
-        if exit:
-            return True
-
-        if fullscreen:
-            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-
-if __name__ == '__main__':
-    main()
+        console.clear()
--- RoguePy/entity.py
+++ RoguePy/entity.py
@@ -1,14 +1,17 @@
+from typing import Tuple
+
+
 class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x, y, char, color):
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
 
-    def move(self, dx, dy):
+    def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
         self.x += dx
         self.y += dy
